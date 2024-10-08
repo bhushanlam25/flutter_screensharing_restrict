d4cb379a2520451d7c4d52a80dcb42c45dd4416a
bhushan.lambole <bhushan.lambole@esds.co.in>
2024-10-08 17:14:50 +0530

    new changes added

--- test/untitled_method_channel_test.dart
+++ test/untitled_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:untitled/MethodChannelFlutterScreensharingRestrict.dart';
+import 'package:flutter_screensharing_restrict/MethodChannelFlutterScreensharingRestrict.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +9,7 @@ void main() {
   const MethodChannel channel = MethodChannel('untitled');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger.setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
         return '42';
@@ -18,7 +18,7 @@ void main() {
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance?.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
